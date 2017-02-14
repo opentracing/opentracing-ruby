75528661bf98e8f99aa0f21137b323132a902c62
Ben Sigelman <bhs@lightstep.com>
2017-02-13 23:07:28 -0800

    PR updates

--- lib/opentracing/tracer.rb
+++ lib/opentracing/tracer.rb
@@ -11,15 +11,15 @@ module OpenTracing
     # @param start_time [Time] When the Span started, if not now
     # @param tags [Hash] Tags to assign to the Span at start time
     # @return [Span] The newly-started Span
-    def start_span(operation_name, child_of: nil, start_time: nil, tags: nil)
+    def start_span(operation_name, child_of: nil, start_time: Time.now, tags: nil)
       Span::NOOP_INSTANCE
     end
 
     # Inject a SpanContext into the given carrier
     #
-    # @param spancontext [SpanContext]
+    # @param span_context [SpanContext]
     # @param format [OpenTracing::FORMAT_TEXT_MAP, OpenTracing::FORMAT_BINARY, OpenTracing::FORMAT_RACK]
-    # @param carrier [Hash]
+    # @param carrier [Carrier] A carrier object of the type dictated by the specified `format`
     def inject(span_context, format, carrier)
       case format
       when OpenTracing::FORMAT_TEXT_MAP, OpenTracing::FORMAT_BINARY, OpenTracing::FORMAT_RACK
@@ -32,7 +32,7 @@ module OpenTracing
     # Extract a SpanContext in the given format from the given carrier.
     #
     # @param format [OpenTracing::FORMAT_TEXT_MAP, OpenTracing::FORMAT_BINARY, OpenTracing::FORMAT_RACK]
-    # @param carrier [Carrier] The carrier type dictated by the specified format
+    # @param carrier [Carrier] A carrier object of the type dictated by the specified `format`
     # @return [SpanContext] the extracted SpanContext or nil if none could be found
     def extract(format, carrier)
       case format
