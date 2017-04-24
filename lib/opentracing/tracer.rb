4096fa24fdeff2be148d5fcd8611926961d9eac0
Indrek Juhkam <indrek@urgas.eu>
2017-04-24 18:55:51 +0300

    Add follows_from option to Tracer#start_span
    Most of the documentation is from
    https://github.com/opentracing/opentracing-go/blob/master/tracer.go#L169

--- lib/opentracing/tracer.rb
+++ lib/opentracing/tracer.rb
@@ -1,17 +1,50 @@
 module OpenTracing
   class Tracer
-    # TODO(bhs): Support FollowsFrom and multiple references
-
     # Starts a new span.
     #
     # @param operation_name [String] The operation name for the Span
-    # @param child_of [SpanContext, Span] SpanContext that acts as a parent to
-    #        the newly-started Span. If a Span instance is provided, its
-    #        context is automatically substituted.
+    #
+    # @param child_of [SpanContext, Span] child_of (ChildOf) refers to a
+    #   parent Span that caused *and* somehow depends upon the new child Span.
+    #   Often (but not always), the parent Span cannot finish until the child
+    #   Span does.
+    #
+    #   An timing diagram for a child Span that is blocked on the new Span:
+    #
+    #     [-Parent Span----------]
+    #          [-Child Span----]
+    #
+    #   See http://opentracing.io/documentation/pages/spec
+    #
+    # @param follows_from [SpanContext, Span] follows_from (FollowsFrom)
+    #   refers to a parent Span that does not depend in any way on the result
+    #   of the new child Span. For instance, one might use FollowsFrom Span to
+    #   describe pipeline stages separated by queues, or a fire-and-forget
+    #   cache insert at the tail end of a web request.
+    #
+    #   A FollowsFrom Span is part of the same logical trace as the new Span:
+    #   i.e., the new Span is somehow caused by the work of its FollowsFrom
+    #   Span.
+    #
+    #   All of the following could be valid timing diagrams for children that
+    #   "FollowFrom" a parent:
+    #
+    #     [-Parent Span--]  [-Child Span-]
+    #
+    #     [-Parent Span--]
+    #      [-Child Span-]
+    #
+    #     [-Parent Span-]
+    #                [-Child Span-]
+    #
+    #   See http://opentracing.io/documentation/pages/spec
+    #
     # @param start_time [Time] When the Span started, if not now
+    #
     # @param tags [Hash] Tags to assign to the Span at start time
+    #
     # @return [Span] The newly-started Span
-    def start_span(operation_name, child_of: nil, start_time: Time.now, tags: nil)
+    def start_span(operation_name, child_of: nil, follows_from: nil, start_time: Time.now, tags: nil)
       Span::NOOP_INSTANCE
     end
 
