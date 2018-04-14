6a3d5d009bfb7e481fd7ced9adda3dce2a191e99
Matthew Wear <matthew.wear@gmail.com>
2018-04-13 17:45:12 -0700

    Add active_span convenience method to OpenTracing module
    It's a common task to have to access the currently active
    Span and it's slightly cumbersome to type:
    `OpenTracing.scope_manager.active.span`. With
    this convenience method, the currently active Span will be
    accessible through `OpenTracing.active_span`.

--- lib/opentracing.rb
+++ lib/opentracing.rb
@@ -38,6 +38,15 @@ module OpenTracing
     attr_accessor :global_tracer
     def_delegators :global_tracer, :scope_manager, :start_active_span,
                    :start_span, :inject, :extract
+
+    # Convenience method to access to currently active span. This is equivalent
+    # to calling `OpenTracing.scope_manager.active.span`
+    #
+    # @return [Span] the currently active span or Nil
+    def active_span
+      scope = scope_manager.active
+      scope.span if scope
+    end
   end
 end
 
